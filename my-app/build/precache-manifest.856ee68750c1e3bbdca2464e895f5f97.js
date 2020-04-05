adfb4f72f7485b004bb3031df8d469100b900e05
carterlurk <carterlurk@ufl.edu>
2020-04-05 00:46:43 +0000

    something

---
+++ my-app/build/precache-manifest.856ee68750c1e3bbdca2464e895f5f97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e861c3b01fb355f2391751439004bfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a654e055374eda57f08c",
+    "url": "/static/css/main.08fca70b.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a654e055374eda57f08c",
+    "url": "/static/js/main.592f39e1.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "438cf1e5a25d34757723160c50dfc43c",
+    "url": "/static/media/corona-extra-4.438cf1e5.svg"
+  }
+]);
